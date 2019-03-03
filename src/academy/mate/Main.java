c40d9894f2a537517d06314b78754ea4be2c30e1
Yevhen <ibookmack@gmail.com>
2019-03-03 11:56:49 +0200

    added new line

--- src/academy/mate/Main.java
+++ src/academy/mate/Main.java
@@ -3,7 +3,7 @@ package academy.mate;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("Git is cool!");
     }
 
     public void work() {
