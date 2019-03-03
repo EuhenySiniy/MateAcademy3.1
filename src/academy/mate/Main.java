2958e1a15ea4a9b4e9f121d29bc01fc10e13c1a2
Yevhen <ibookmack@gmail.com>
2019-03-03 12:22:55 +0200

    cherry-pick

--- src/academy/mate/Main.java
+++ src/academy/mate/Main.java
@@ -5,10 +5,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Git is cool!");
 
-
+        Main main = new Main();
+        main.work();
+        System.out.println(main.sum(5, 6));
     }
 
     public void work() {
         System.out.println("Now i'm working..");
+    }
+
+    private int sum(int a, int b) {
+        return a + b;
     }
 }
